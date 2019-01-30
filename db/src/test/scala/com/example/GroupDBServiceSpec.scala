2f7c33daaf7bb74c15deb2d136440f6e583b4f62
shubhamdangare <shubham17495@gmail.com>
2019-01-30 12:33:32 +0530

    Added scala test

--- db/src/test/scala/com/example/GroupDBServiceSpec.scala
+++ db/src/test/scala/com/example/GroupDBServiceSpec.scala
@@ -1,49 +1,65 @@
 package com.example
+
 import org.scalatest.WordSpec
+import org.scalatest.{Matchers}
+import org.scalatest.concurrent.ScalaFutures
 
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Success}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-
-class GroupDBServiceSpec extends WordSpec {
-
+class GroupDBServiceSpec extends WordSpec with ScalaFutures with Matchers {
   val groupObj = new GroupDBService
 
   "A Group" should {
-
     "Return value for getGroup" in {
 
-       val result = Await.result(groupObj.getGroup(1),5000 millis)
-       val groupOne = Group(1,"1212aa")
-       assert(result.get == groupOne)
+      whenReady(groupObj.getGroup(1)) {
+        result => result.get shouldEqual Group(1, "1212aa")
+      }
     }
-
 
     "Not Return Empty List of Group" in {
+      val groupList: List[Group] = List[Group]()
+      whenReady(groupObj.getGroups) {
+        result => assert(result != groupList)
+      }
 
-      val result = Await.result(groupObj.getGroups,5000 millis)
-
-
-      val groupList:List[Group] = List[Group]()
-      assert(result != groupList)
     }
 
-
-
     "Return List of Group" in {
-
       val mockedUserGroupList: List[Group] = List(
         Group(1, "1212aa"),
         Group(2, "1212ab"),
         Group(3, "1212ac"),
         Group(4, "1212ad")
       )
+      whenReady(groupObj.getGroups) {
+        result => assert(result === mockedUserGroupList)
+      }
 
-      val result = Await.result(groupObj.getGroups,5000 millis)
-      assert(result === mockedUserGroupList)
     }
-
   }
 }
 
+/**
+  * *
+  * val result = Await.result(groupObj.getGroup(1), 5000 millis)
+  * val groupOne = Group(1, "1212aa")
+  * assert(result.get == groupOne)
+  * *
+  * "Not Return Empty List of Group" in {
+  * val result = Await.result(groupObj.getGroups, 5000 millis)
+  * val groupList: List[Group] = List[Group]()
+  * assert(result != groupList)
+  * }
+  * *
+  * "Return List of Group" in {
+  * val mockedUserGroupList: List[Group] = List(
+  * Group(1, "1212aa"),
+  * Group(2, "1212ab"),
+  * Group(3, "1212ac"),
+  * Group(4, "1212ad")
+  * )
+  * *
+  * val result = Await.result(groupObj.getGroups, 5000 millis)
+  * assert(result === mockedUserGroupList)
+  * }
+  *
+  */
---
+++ db/src/test/scala/com/example/UserDBServiceSpec.scala
@@ -0,0 +1,40 @@
+package com.example
+
+import org.scalatest.WordSpec
+import org.scalatest.{Matchers}
+import org.scalatest.concurrent.ScalaFutures
+
+class UserDBServiceSpec extends WordSpec with ScalaFutures with Matchers {
+  val userObj = new UserDBService
+
+  "A User" should {
+    "Return value for getUser" in {
+      whenReady(userObj.getUser(1)) {
+        result => result.get shouldEqual User(1, "asdasd1", "asdasd1", 1, "abc")
+      }
+    }
+
+    "Not Return Empty List of Users" in {
+      val userList: List[User] = List[User]()
+      whenReady(userObj.getUsers) {
+        result => assert(result != userList)
+      }
+    }
+
+    "Return List of Users" in {
+      val mockedUserList: List[User] = List(
+        User(1, "asdasd1", "asdasd1", 1, "abc"),
+        User(2, "asdasd2", "asdasd2", 1, "abc"),
+        User(3, "asdasd3", "asdasd3", 4, "pqr"),
+        User(4, "asdasd4", "asdasd4", 2, "pqr")
+      )
+
+      whenReady(userObj.getUsers) {
+        result => assert(result === mockedUserList)
+      }
+
+    }
+  }
+}
+
+
