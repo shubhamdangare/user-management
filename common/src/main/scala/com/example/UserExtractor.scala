d2ce3aa415e9c428a4afd98449533a822fde3e40
shubhamdangare <shubham17495@gmail.com>
2019-02-04 18:05:55 +0530

    Added DSL Quesry with Routes

--- common/src/main/scala/com/example/UserExtractor.scala
+++
@@ -1,4 +0,0 @@
-
-package com.example
-
-final case class UserExtractor(name:String, password:String, groupId:String, permission:String )
---
+++ common/src/main/scala/com/example/UserRequest.scala
@@ -0,0 +1,4 @@
+
+package com.example
+
+final case class UserRequest(name:String, password:String, groupId:String, permission:String )
