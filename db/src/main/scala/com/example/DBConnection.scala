fd6df26016f73dc836d5d8094111b0e14e9ab507
shubhamdangare <shubham17495@gmail.com>
2019-02-04 19:22:46 +0530

    Added DSL Quesry with Routes

---
+++ db/src/main/scala/com/example/DBConnection.scala
@@ -0,0 +1,12 @@
+
+package com.example
+
+import scalikejdbc._
+
+class DBConnection {
+
+  def createConnectiontoDB():Unit = {
+    Class.forName("com.mysql.jdbc.Driver")
+    ConnectionPool.singleton("jdbc:mysql://localhost:3306/usermanagement", "root", "shubham")
+  }
+}
